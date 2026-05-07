475d4bbd90c19315c754b838d0e1cb19bbca964c
NavyaPachigolla <24b01a12b5@svecw.edu.in>
2026-05-07 18:25:32 +0530

    final working chatbot

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import streamlit as st
 import requests
 import os
 
-st.title("🤖 Online AI Chatbot")
+st.title("🤖 AI Chatbot")
 
+# Get API key from Render environment variable
 API_KEY = os.getenv("OPENROUTER_API_KEY")
 
+# OpenRouter API URL
 url = "https://openrouter.ai/api/v1/chat/completions"
 
 headers = {
@@ -13,14 +15,18 @@ headers = {
     "Content-Type": "application/json"
 }
 
-user_input = st.text_input("Ask something:")
+# User input
+user_input = st.text_input("Ask me anything")
 
 if user_input:
 
     payload = {
-        "model": "openchat/openchat-7b",
+        "model": "meta-llama/llama-3.1-8b-instruct:free",
         "messages": [
-            {"role": "user", "content": user_input}
+            {
+                "role": "user",
+                "content": user_input
+            }
         ]
     }
 
@@ -28,8 +34,10 @@ if user_input:
 
     data = response.json()
 
+    # Display response
     try:
         answer = data["choices"][0]["message"]["content"]
-        st.write("🤖 Answer:", answer)
+        st.write("🤖", answer)
+
     except:
         st.write(data)
